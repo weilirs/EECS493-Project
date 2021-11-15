6c7087cef38ea5161f02cfb6f7d55ef1fd78aa19
Tyler Sadama <tsadama@umich.edu>
2021-11-14 22:35:02 -0500

    add create page html

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 Vue.config.devtools = true
-var resultView = new Vue({
+var createPageView = new Vue({
     el: '#app',
+    data: {
+        showCreate: false,
+        showHome: true
+    },
+    methods: {
+        showCreatePage () {
+            console.log("Hi");
+            this.showCreate = true;
+            this.showHome = false;
+        }
+    }
 })
