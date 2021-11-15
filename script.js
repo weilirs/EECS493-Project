e22c7c37de7b80de26f9b707dcd3c8ff3f492af7
weilirs <weilirs@umich.edu>
2021-11-15 18:57:50 -0500

    bind variables, add "back" buttons

--- script.js
+++ script.js
@@ -3,13 +3,29 @@ var createPageView = new Vue({
     el: '#app',
     data: {
         showCreate: false,
-        showHome: true
+        showJoin: false,
+        showHome: true,
+        eventName: null,
+        yourName: null,
+        address: null,
+        date: null,
+        time: null,
+        gameDscp: null,
     },
     methods: {
         showCreatePage () {
-            console.log("Hi");
             this.showCreate = true;
             this.showHome = false;
+        },
+
+        showJoinPage () {
+            this.showJoin = true;
+            this.showHome = false;
+        },
+
+        createPageBack () {
+            this.showCreate = false;
+            this.showHome = true;
         }
     }
 })
