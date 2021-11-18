1da81820904d03e1734785bdf766651064620e50
Tyler Sadama <tsadama@umich.edu>
2021-11-17 20:37:42 -0500

    modifies create event page and adds functionality

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ var createPageView = new Vue({
     el: '#app',
     data: {
         showCreate: false,
+        showCreateSuccess: false,
         showJoin: false,
         showHome: true,
         eventName: null,
+        eventType: null,
         yourName: null,
         address: null,
         date: null,
         time: null,
         gameDscp: null,
+        events: []
     },
     methods: {
         showCreatePage () {
@@ -26,6 +29,35 @@ var createPageView = new Vue({
         createPageBack () {
             this.showCreate = false;
             this.showHome = true;
+        },
+
+        joinPageBack () {
+            this.showJoin = false;
+            this.showHome = true;
+        },
+
+        clearForm () {
+            this.eventName = null;
+            this.eventType = null;
+            this.yourName = null;
+            this.address = null;
+            this.date = null;
+            this.time = null;
+            this.gameDscp = null;
+            $('#eventForm')[0].reset();
+        },
+
+        createSuccessHome () {
+            this.showCreateSuccess = false;
+            this.showHome = true;
+            this.clearForm();
+        },
+
+        createEventHandler () {
+            let event = {eventName: this.eventName, type: this.eventType, yourName: this.yourName, address: this.address, date: this.date, time: this.time, description: this.gameDscp};
+            this.events.push(event);
+            this.showCreate = false;
+            this.showCreateSuccess = true;
         }
     }
 })
